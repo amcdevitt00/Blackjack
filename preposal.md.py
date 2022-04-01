334e7ca5e8cf7edbc90a996ef296d4ae9816b5fb
amcdevitt00 <102620303+amcdevitt00@users.noreply.github.com>
2022-04-01 10:56:38 -0400

    Add files via upload

---
+++ preposal.md.py
@@ -0,0 +1,13 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+Blackjack game that counts cards and tells you when to go in based on probability and which cards are left in deck. 
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+Daniel Hedberg
+
+## Do you have any questions of your own?
+
+Can we write a code that would interact with a raspberry pi computer and pi camera without actually purchasing and using the hardware?
