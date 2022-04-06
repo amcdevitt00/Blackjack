42dd3d5e05a17adab1229285bc619e215d695cd5
daniel-hedberg <102620472+daniel-hedberg@users.noreply.github.com>
2022-04-06 14:57:45 -0400

    Update proposal.py

--- proposal.py
+++ proposal.py
@@ -10,7 +10,7 @@ Blackjack card-counting code.
 
 ## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
 
-Our project will begin with a function that shuffles 5 decks of cards and outputs them into 260 different dictionaries. After each card is drawn, the user will be told whether it is a good idea to bet more, based on how many high cards compared to low cards there are. The user will also be notified when this amount crosses a certain threshold.  
+Our project will begin with a function that shuffles 6-8 decks of cards and outputs each card into different dictionaries. After each card is drawn, the user will be told whether the remaining deck is hot or cold, based on how many high cards compared to low cards there are left in the deck. The user will also be notified when this amount crosses a certain threshold.  
 
 ## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
 
@@ -33,8 +33,6 @@ Black jack card-counter keeping a count of different types of cards. When the de
 
 ### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
 
-Black jack card-counter that visually shows the cards in play while keeping a count of different types of cards. When the deck, based on probability, has many high-value cards left, user will be notified.
+Black jack card-counter that visually shows when the remaining deck is hot or cold and notifies the user when it has been hot for a certain amount of time. Additionally, statisitcs will be gathered on the odds of decks running hot for more than a ceratin amount of time and how often that occurs in a randomly shufles deck of 6-8 decks.
 
 ## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
-
-TODO
