0a7268fefcdfbdf430e3b669b56478eba86801ec
amcdevitt00 <102620303+amcdevitt00@users.noreply.github.com>
2022-04-06 09:07:50 -0400

    Add files via upload

---
+++ proposal.py
@@ -0,0 +1,40 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+Blackjack hot deck
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+Blackjack card-counting code.
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+Our project will begin with a function that shuffles 5 decks of cards and outputs them into 260 different dictionaries. After each card is drawn, the user will be told whether it is a good idea to bet more, based on how many high cards compared to low cards there are. The user will also be notified when this amount crosses a certain threshold.  
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+no
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+Aaron McDevitt    aaron.mcdevitt@temple.edu    Jeremy Fan
+Daniel Hedberg    daniel.hedberg@temple.edu    Jeremy Fan
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+Black jack card-counter keeping a count of different types of cards.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+Black jack card-counter keeping a count of different types of cards. When the deck, based on probability, has many high-value cards left, user will be notified.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+Black jack card-counter that visually shows the cards in play while keeping a count of different types of cards. When the deck, based on probability, has many high-value cards left, user will be notified.
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+TODO
