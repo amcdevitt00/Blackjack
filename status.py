085bffcf7370f766121bb751e9498935627ecbcd
amcdevitt00 <102620303+amcdevitt00@users.noreply.github.com>
2022-04-12 14:45:17 -0400

    Add files via upload

---
+++ status.py
@@ -0,0 +1,31 @@
+# Status Report
+
+#### Your name
+
+Aaron McDevitt, Daniel Hedberg
+
+#### Your section leader's name
+
+Jeremy Fan
+
+#### Project title
+
+Blackjack Hot Deck
+
+***
+
+Short answers for the below questions suffice. If you want to alter your plan for your project (and obtain approval for the same), be sure to email your section leader directly!
+
+We will not be showing a visual of the cards in play. We instead will be utilizing a thermometer that tells the user when their probability of winning is greater (or when the deck is "hot"). We will also be able to run the game many times and output stats on what percentage of time the user had a greater chance of winning. 
+
+#### What have you done for your project so far?
+
+We have imported a thermometer, otherwise known as a "hot meter," which in real time tells the user the probability of winning the hand. The meter starts at dark blue which indicates a 0%-49% chance of winning, light blue = 50%-51%, green = 51%-52%, orange = 52%-53%, and red = <53%. The code also outputs the percent of time the meter was in each of these ranges, and records how long the code took to run. 
+
+#### What have you not done for your project yet?
+
+We still need to write an additional code to compile statistics from the outcome of numerous games played.
+
+#### What problems, if any, have you encountered?
+
+Having trouble with the thermometer visual.
